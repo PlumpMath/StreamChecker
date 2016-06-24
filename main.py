d5b65e41490c137167e16b183999e1a6b8e43c53
Chris Zimmerman <chris-zimmerman@live.com>
2016-06-24 02:52:29 -0400

    Adding initial files, currently check if streams are online

---
+++ main.py
@@ -0,0 +1,25 @@
+import json
+from urllib.request import urlopen
+import string
+
+twitchBase = "https://api.twitch.tv/kraken/streams/"
+
+with open("streams.json") as json_file:
+    json_data = json.load(json_file)
+
+def get_jsonparsed_data(url):
+    response = urlopen(url)
+    data = str(response.read())
+    data = data.encode('ascii','ignore').decode('unicode_escape')
+    data = data[2:-1]
+    return json.loads(data)
+
+for x in range(0, len(json_data["streams"])):
+    urlToCheck = twitchBase + json_data["streams"][x]["streamURL"]
+    response = get_jsonparsed_data(urlToCheck)
+    if(response["stream"] != None):
+        print((json_data["streams"][x]["streamURL"]) + " is active")
+    else:
+        print((json_data["streams"][x]["streamURL"]) + " is not active")
+    
+
